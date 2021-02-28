bdfc8494eab89b4e921576b395cc417c35287918
Jowooseok <whdntjrsla12@naver.com>
2021-02-28 22:06:22 +0900

    study java8 #1

---
+++ src/Foo.java
@@ -0,0 +1,6 @@
+public class Foo {
+    public static void main(String[] args) {
+        int baseNumber = 10;
+        RunSometing runSometing = () -> System.out.println("hi");
+    }
+}
---
+++ src/RunSometing.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface RunSometing {
+    void doit();
+}
