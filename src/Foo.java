5dc514f458ade89bfb87f3f1c61ae16ad8646f44
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-03-02 15:52:39 +0900

    람다 표현식

--- src/Foo.java
+++ src/Foo.java
@@ -1,6 +1,15 @@
+import java.util.function.Function;
+
 public class Foo {
     public static void main(String[] args) {
-        int baseNumber = 10;
-        RunSometing runSometing = () -> System.out.println("hi");
+        Function<Integer, Integer> plus10 = (i) -> i+10;
+        Function<Integer, Integer> multiple2 = (i) -> i*2;
+
+        Function<Integer, Integer> plus10AndMultiple2 = plus10.compose(multiple2); //compse는 먼저 실행하는 함수
+        Function<Integer, Integer> multiple2AndPlus10 = plus10.andThen(multiple2); //andthen은 나중에  실행하는 함수
+        System.out.print(plus10AndMultiple2.apply(2)); //14
+        System.out.print(multiple2AndPlus10.apply(2)); //24
+
     }
 }
+ 
